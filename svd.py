b6e8535190dcf472dbf86490b754745ebdf19c8b
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-07-10 00:16:14 +0300

    fixed svd
    - handled non-symmetric matrices

--- svd.py
+++ svd.py
@@ -3,20 +3,33 @@ import numpy as np
 REGULARIZATION_CONSTANT = 1e-10
 
 def decompose(A):
-    eigenvalues, eigenvectors = np.linalg.eigh(np.dot(A.T, A))  # for right singular vectors V
-    eigenvalues = np.abs(eigenvalues)
-    idx = np.argsort(eigenvalues)[::-1]
-    eigenvalues = eigenvalues[idx]
-    V = eigenvectors[:, idx]  # eigenvectors are the columns of the matrix
+    U, eigenvalues = findSingularVectors(A)
+    V = findSingularVectors(A.T)[0]
     
     singular_values = np.sqrt(eigenvalues)
     S = np.zeros(A.shape)
-    np.fill_diagonal(S, singular_values[:min(A.shape)])
-
-    S_inverse = np.diag(1.0 / singular_values)
-    U = np.dot(A, np.dot(V, S_inverse))[:, :min(A.shape)]
-    U = U / (np.linalg.norm(U, axis=0) + REGULARIZATION_CONSTANT)
+    np.fill_diagonal(S, singular_values[:A.shape[0]])
+        
+    if A.shape[0] > A.shape[1]:
+        V = coordinate_vectors(V, A.T, U, singular_values)
+    else:
+        U = coordinate_vectors(U, A, V, singular_values)
+        
     return U, S, V.T
+
+
+def findSingularVectors(A):
+    eigenvalues, eigenvectors = np.linalg.eigh(np.dot(A, A.T)) 
+    eigenvalues = np.abs(eigenvalues)
+    idx = np.argsort(eigenvalues)[::-1]
+    eigenvalues = eigenvalues[idx]
+    return eigenvectors[:, idx], eigenvalues
+
+
+def coordinate_vectors(L, A, R, singular_values):
+    for i in range(L.shape[0]):
+        L[:, i] = np.dot(A, R[:, i]) * (1 / singular_values[i])
+    return L
 
 
 def verify(A, U, S, V_T):
